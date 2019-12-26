118b1de0bddffe7528854b3275c26fda3d90749d
RahinF <s3297352@student.rmit.edu.au>
2019-12-26 16:36:35 +1100

    created logo, added 3 fonts (company, headers, text), fixed seating section layout, improved now showing section (layout flows better), added 1 movie poster, added all of the synopsis section

--- a2/index.php
+++ a2/index.php
@@ -9,13 +9,17 @@
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     <script src='../wireframe.js'></script>
-  </head>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Courgette|Montserrat|Tajawal&display=swap" rel="stylesheet">
+    </head>
 
   <body>
 
     <header>
       <div>
-        <img src="../../media/Logo.jpg" alt="Lunardo Logo">
+        <!-- logo from https://www.logomaker.com-->
+        <img src="../../media/logo.png" alt="Lunardo Logo">
         <h1>Lunardo</h1>
       </div>
     </header>
@@ -36,48 +40,52 @@
 
         <div class="hero">
           <div class="hero-text">
-            <h1>About us</h1>
+            <h1>Lunardo cinema grand reopening</h1>
             <p>reopening Lunardo Cinema now sport powerful dolby sound system, advanced stuff</p>
           </div>
         </div>
+        <div >
 
 
+
+
+        <div class="chairs-info">
+          <h1>New and improved seating</h1>
+          <p>Now introducing our brand new seating options. Experience different levels of comfort with our latest standards and premium seating options.</p>
+        </div>
 
         <div class="flex-container">
 
-          <div class="chairs_info">
-            <h1>chairs</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-
-          <div class="chairs_standard">
-            <img  height= '400px' src="../../media/538.jpg" alt="Standard seats">
+          <div class="chairs-standard">
+            <img height= '400px' src="../../media/538.jpg" alt="Standard seats">
             <div class="chair-text">
               <h2>Standard seats</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
           </div>
 
-            <div class="chairs_premium">
+            <div class="chairs-premium">
               <img  height= '400px' src="../../media/Verona-Twin.png" alt="Standard seats">
               <div class="chair-text">
-                <h2>premium seats</h2>
+                <h2>Premium seats</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
             </div>
 
           </div>
 
-        <div>
-          <img src="../../media/Cinema.jpg" alt="temp">
-          <h1>projection system</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div class="projection-system">
+          <div class="projection-system-text">
+            <h1>projection system</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
         </div>
       </section>
 
       <section id="Prices">
         <h1>Prices</h1>
-      On x days ticket prices are discounted!
+        <p>On x days ticket prices are discounted!</p>
+
 
 
 
@@ -153,10 +161,10 @@ all day Sunday
 
             <h1>Now showing</h1>
 
-            <div class="flex-container">
+            <div class="flex-container-movies">
 
               <div class="movie-box">
-                <img height= '400px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
+                <img height= '300px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
               </div>
               <div>
                 <h4>Star Wars: The Rise of Skywalker</h4>
@@ -173,78 +181,87 @@ all day Sunday
               </div>
 
               <div class="movie-box">
-                <img height= '400px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
+                <img height= '300px'  width="200px" src="../../media/Frozen-2.jpg" alt="Star Wars: The Rise of Skywalker movie poster">
               </div>
               <div>
-                <h4>Star Wars: The Rise of Skywalker</h4>
-                <h5>Rating: M</h5>
+                <h4>Frozen 2</h4>
+                <h5>Rating: PG</h5>
                 <p>
-                  Monday 12:00pm <br>
-                  Tuesday 12:00pm <br>
-                  Wednesday 6:00pm <br>
-                  Thursday 6:00pm <br>
-                  Friday 6:00pm <br>
-                  Saturday 12:00pm <br>
-                  Sunday 12:00pm
+                  Monday - <br>
+                  Tuesday - <br>
+                  Wednesday 9:00pm <br>
+                  Thursday 9:00pm <br>
+                  Friday 9:00pm <br>
+                  Saturday 6:00pm <br>
+                  Sunday 6:00pm
                 </p>
               </div>
 
               <div class="movie-box">
-                <img height= '400px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
+                <img height= '300px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
               </div>
               <div>
-                <h4>Star Wars: The Rise of Skywalker</h4>
-                <h5>Rating: M</h5>
+                <h4>The Aeronauts</h4>
+                <h5>Rating: PG</h5>
                 <p>
-                  Monday 12:00pm <br>
-                  Tuesday 12:00pm <br>
-                  Wednesday 6:00pm <br>
-                  Thursday 6:00pm <br>
-                  Friday 6:00pm <br>
-                  Saturday 12:00pm <br>
-                  Sunday 12:00pm
+                  Monday 6:00pm <br>
+                  Tuesday 6:00pm <br>
+                  Wednesday - <br>
+                  Thursday - <br>
+                  Friday - <br>
+                  Saturday 3:00pm <br>
+                  Sunday 3:00pm
                 </p>
               </div>
 
               <div class="movie-box">
-                <img height= '400px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
+                <img height= '300px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
               </div>
                 <div>
-                  <h4>Star Wars: The Rise of Skywalker</h4>
-                  <h5>Rating: M</h5>
+                  <h4>JoJo Rabbit</h4>
+                  <h5>Rating: PG</h5>
                   <p>
-                    Monday 12:00pm <br>
-                    Tuesday 12:00pm <br>
-                    Wednesday 6:00pm <br>
-                    Thursday 6:00pm <br>
-                    Friday 6:00pm <br>
-                    Saturday 12:00pm <br>
-                    Sunday 12:00pm
+                    Monday - <br>
+                    Tuesday - <br>
+                    Wednesday 12:00pm <br>
+                    Thursday 12:00pm <br>
+                    Friday 12:00pm <br>
+                    Saturday 9:00pm <br>
+                    Sunday 9:00pm
                   </p>
                 </div>
 
             </div> <!-- end of flex-container -->
 
           <!--large display-->
-          <div class="flex-container">
 
-            <div class="large-box">
-                <img height= '400px' src="../../media/SW_Poster.png" alt="Star Wars: The Rise of Skywalker movie poster">
-            </div>
+
               <div class="showcase-details">
-                <h4>Star Wars: The Rise of Skywalker</h4>
-                <h5>Rating: M</h5>
-                <p>
-                  Monday 12:00pm <br>
-                  Tuesday 12:00pm <br>
-                  Wednesday 6:00pm <br>
-                  Thursday 6:00pm <br>
-                  Friday 6:00pm <br>
-                  Saturday 12:00pm <br>
-                  Sunday 12:00pm
-                </p>
+                <div class="flex-container">
+
+                <div class="showcase-text">
+                  <h4>Star Wars: The Rise of Skywalker</h4>
+                  <h5>Rating: M</h5>
+                  <p>The surviving Resistance faces the First Order once more as Rey, Finn and Poe Dameron's journey continues. With the power and knowledge of generations behind them, the final battle commences.</p>
+                  <ul>
+                    <li>Monday 12:00pm</li>
+                    <li>Tuesday 12:00pm</li>
+                    <li>Wednesday 6:00pm</li>
+                    <li>Thursday 6:00pm</li>
+                    <li>Friday 6:00pm</li>
+                    <li>Saturday 12:00pm</li>
+                    <li>Sunday 12:00pm</li>
+                  </ul>
+
+                </div>
+
+                <div>
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/14EztMXh5vQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
               </div>
             </div>
+
+
 
 
           </section>
