9732c4a907a6054016fde2146bb9496c52b71986
RahinF <s3297352@student.rmit.edu.au>
2020-02-14 12:21:41 +1100

    receipt page mostly completed (need to fix extra page error on print and reflow)

--- a4/receipt.php
+++ a4/receipt.php
@@ -59,13 +59,19 @@ function getSeatName($seat){
   }
 }
 
-//echo $movieID;
-// display individual tickets
-function showTickets(){
-  foreach($_SESSION['seats'] as $seat){
-    if ($seat > 0){
-      for($i = 0; $i < $seat; $i++){
 
+// display individual tickets
+
+function showTickets(){
+  $ticketCount =0;
+  foreach($_SESSION['seats'] as $seat){
+
+    if ($seat > 0){
+
+      for($i = 0; $i < $seat; $i++){
+        if ($ticketCount == 0){
+      echo '<div class="ticket-section">';
+    }
       echo  '<div class="ticket">';
       echo  '<div class="ticket-a">';
       echo  '<h1>Lunardo Cinema</h1>';
@@ -77,7 +83,12 @@ function showTickets(){
       echo  '<img src="../../media/barcode.png" alt="">';
       echo  '</div>';
       echo  '</div>';
+      $ticketCount++;
+      if ($ticketCount == 10){
+        echo  '</div>';
+        $ticketCount = 0;
 
+      }
       }
     }
   }
@@ -96,16 +107,18 @@ function showTickets(){
   <!-- Keep wireframe.css for debugging, add your css to style.css -->
   <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
   <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+  <link rel="stylesheet" href="receipt-print.css">
   <script src='../wireframe.js'></script>
 
 
   <!-- Google font -->
   <link href="https://fonts.googleapis.com/css?family=Courgette|Montserrat|Tajawal&display=swap" rel="stylesheet">
 </head>
+
   <body>
 <div class="receipt-box">
-<div id="receipt">
-  <div class="receipt-header">
+  <div id="receipt">
+    <div class="receipt-header">
     <h1>Lunardo</h1>
     <span class="lunardo-info">
     <p>
@@ -115,8 +128,7 @@ function showTickets(){
     </p>
     </span>
   </div>
-
-<div class = "receipt-text">
+    <div class = "receipt-text">
   <div class="receipt-customer-details">
     <h2>Customer details</h2>
     <hr>
@@ -124,28 +136,21 @@ function showTickets(){
     <p>Mobile: <?php echo $_SESSION["cust"]["mobile"]; ?></p>
     <p>Email: <?php echo $_SESSION["cust"]["email"]; ?></p>
   </div>
-
-
-<div class="receipt-movie-details">
+    <div class="receipt-movie-details">
   <h2>Movie details</h2>
   <hr>
   <p>Movie: <?php echo getMovieName($_SESSION["movie"]["id"]);?></p>
   <p>Session: <?php echo $_SESSION["movie"]["day"]." ".$_SESSION["movie"]["hour"]; ?></p>
 </div>
 
-
-
   <?php
-
   echo  '<table>';
   echo  '<tr>';
   echo  '<th>Quantity</th>';
   echo  '<th>Ticket Type</th>';
   echo  '</tr>';
   foreach($_SESSION['seats'] as $seat){
-
     if ($seat > 0){
-
       for($i = 0; $i < count($seat); $i++){
         echo  '<tr>';
         echo '<td>';
@@ -156,27 +161,24 @@ function showTickets(){
         echo'</td>';
         echo  '</tr>';
       }
-
     }
   }
   echo  '</table>';
   ?>
-<div class="receipt-cost-details">
-
-  <p>Cost: $<?php echo $totalCost = sprintf("%.2f",(float)$_SESSION["cost"]); ?></p>
-  <p>GST: $<?php echo $gst = sprintf("%.2f",$totalCost/11); ?></p>
-  <hr>
-  <p>Total: $<?php echo $totalCost + $gst; ?></p>
-  <hr>
+<div class="thank-you-message">
+  <p>Thank you for your purchase! Your tickets are available below.</p>
 </div>
-
+  <div class="receipt-cost-details">
+    <p>Cost: $<?php echo $totalCost = sprintf("%.2f",(float)$_SESSION["cost"]); ?></p>
+    <p>GST: $<?php echo $gst = sprintf("%.2f",$totalCost/11); ?></p>
+    <hr>
+    <p>Total: $<?php echo $totalCost + $gst; ?></p>
+    <hr>
+  </div>
  </div>
 </div>
 
-<div class="ticket-section"><?php showTickets(); ?></div>
-
-
+<?php showTickets(); ?>
 </div>
-
   </body>
 </html>
